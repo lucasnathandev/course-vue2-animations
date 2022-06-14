0f9ae077344364f33509883cf2c938abbc33e1de
Solium Dev <lucas.develop.prog@gmail.com>
2022-06-14 15:11:30 -0300

    Transitioning state with numbers and GSAP lib for animations. Section finished.

--- src/App.vue
+++ src/App.vue
@@ -9,91 +9,50 @@
       </div>
     </div>
     <div class="container">
-      <h3 class="font-weight-light">Technologies</h3>
-
-      <div class="row">
-        <div class="col-sm-2">
-          <button class="btn btn-info" @click="shuffle">Embaralhar</button>
-        </div>
-        <div class="col-sm-10">
-          <div class="form-group">
-            <label for=""></label>
-            <input
-              @keyup.enter.exact="add"
-              ref="input"
-              type="text"
-              name=""
-              id=""
-              class="form-control"
-              placeholder="Inser a new item and press ENTER"
-              aria-describedby="helpId"
-            />
-            <small id="helpId" class="text-muted">Help text</small>
-          </div>
-        </div>
+      <h3 class="font-weight-light">State animations / Transitioning State</h3>
+      <div class="form-group">
+        <label for="input-number"></label>
+        <input
+          v-model="number"
+          type="number"
+          name=""
+          step="50"
+          id="input-number"
+          class="form-control"
+          placeholder=""
+          aria-describedby="helpId"
+        />
       </div>
-
-      <transition-group tag="ul" class="list-group" :name="'list'">
-        <li
-          class="list-group-item"
-          v-for="(technology, index) in technologies"
-          :key="technology"
-        >
-          <span>{{ technology }}</span>
-          <button
-            class="btn btn-danger btn-sm float-right"
-            @click="remove(index)"
-          >
-            X
-          </button>
-        </li></transition-group
-      >
+      <div class="alert alert-info">
+        <h3 class="font-weight-light">
+          <strong>Number: </strong><span>{{ animatedNumber }}</span>
+        </h3>
+      </div>
     </div>
   </div>
 </template>
 
 <script>
-import lodash from "lodash";
+import gsap from "gsap";
 export default {
   name: "App",
   data() {
     return {
-      technologies: ["JavaScript", "VueJS", "VueX", "VueRouter"],
+      number: 0,
+      interpolatedNumber: 0,
     };
   },
-  methods: {
-    add(event) {
-      const newItem = event.target.value;
-      if (newItem) {
-        const index = Math.floor(Math.random() * this.technologies.length);
-        this.technologies.splice(index, 0, newItem);
-        this.$refs.input.value = "";
-      }
+  computed: {
+    animatedNumber() {
+      return this.interpolatedNumber.toFixed(0);
     },
-    remove(index) {
-      this.technologies.splice(index, 1);
-    },
-    shuffle() {
-      this.technologies = lodash.shuffle(this.technologies);
+  },
+  watch: {
+    number(current) {
+      gsap.to(this.$data, 2, {
+        interpolatedNumber: current,
+      });
     },
   },
 };
 </script>
-<style scoped>
-.list-enter,
-.list-leave-to {
-  opacity: 0;
-  transform: translateX(-70px);
-}
-
-.list-enter-active,
-.list-leave-active,
-/* New feature */ .list-move {
-  transition: all 1s ease-in-out;
-}
-
-.list-leave-active {
-  position: absolute;
-  width: calc(100% - 100px);
-}
-</style>
